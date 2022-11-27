e6e683092b2cc51055ac3c1d312e94c1bf4f268e
kirinokirino <kirinokirino@lunariko.com>
2022-11-27 19:23:57 +0300

    Clicking the mouse creates rectangles.

--- mylib.lua
+++ mylib.lua
@@ -1,5 +1,5 @@
 
-love.graphics.rect = function(rect)
+draw_rect = function(rect)
   love.graphics.rectangle('line', rect.x, rect.y, rect.w, rect.h)
 end
 
@@ -10,7 +10,42 @@ point = function(x, y)
   return {["x"] = x, ["y"] = y}
 end
 
-rect = function(p, w, h)
+rect = function(a1, a2, a3) 
+  if (type(a2) == 'number') then
+    -- treating a2 as width and a3 as height
+    return rect1(a1, a2, a3)
+  else
+    assert(type(a3) == 'nil')
+    -- treating a1 and a2 as points
+    return rect2(a1, a2)
+    end
+end
+
+rect2 = function(p1, p2)
+  --- creates a rectangle from two points
+  assert(type(p1.x) == "number")
+  assert(type(p1.y) == "number")
+  assert(type(p2.x) == "number")
+  assert(type(p2.y) == "number")
+  local min_x, min_y, max_x, max_y;
+  min_x = math.min(p1.x, p2.x)
+  min_y = math.min(p1.y, p2.y)
+  max_x = math.max(p1.x, p2.x)
+  max_y = math.max(p1.y, p2.y)
+  return {
+    ["x"] = min_x, ["y"] = min_y, ["w"] =  max_x - min_x, ["h"] = max_y - min_y, 
+    points = function(self) 
+      return { 
+        point(self.x, self.y), 
+        point(self.x + self.w, self.y),
+        point(self.x + self.w, self.y + self.h), 
+        point(self.x, self.y + self.h)
+      }
+    end,
+    }
+end
+
+rect1 = function(p, w, h)
   --- creates a rectangle with origin of point p and dimensions of w and h
   --- p: origin point (top left corner)
   --- w, h: rectangle dimensions (numbers)
@@ -27,7 +62,7 @@ rect = function(p, w, h)
         point(self.x + self.w, self.y + self.h), 
         point(self.x, self.y + self.h)
       }
-    end
+    end,
     }
 end
 
@@ -35,14 +70,19 @@ polygon = function(world, points)
   --- creates a physical polygon and adds it to the world
   local polygon = {}
   local pts = {}
+  local origin = {}
   assert(type(points) == "table")
   for i, point in ipairs(points) do
     assert(type(point.x) == "number")
     assert(type(point.y) == "number")
-    pts[i*2-1] = point.x
-    pts[i*2] = point.y
+    if (i == 1) then
+      origin = point
+    end
+    pts[i*2-1] = point.x - origin.x
+    pts[i*2] = point.y - origin.y
   end
-  polygon.body = love.physics.newBody(world, pts[0], pts[1], 'dynamic')
+  
+  polygon.body = love.physics.newBody(world, origin.x, origin.y, 'dynamic')
   polygon.shape = love.physics.newPolygonShape(unpack(pts))
   polygon.fixture = love.physics.newFixture(polygon.body, polygon.shape)
   return polygon
